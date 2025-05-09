5329b29ef628bf3e3bdd69a8c3cb88118092611c
Miguel Quintero <miguel.quinterov04@gmail.com>
2025-05-08 23:47:05 -0500

    Creacion de la Raiz del APP

---
+++ app/index.tsx
@@ -0,0 +1,7 @@
+import SplashScreen from "@/src/layouts/LoginScreen";
+
+
+export default function Index() {
+  return <SplashScreen/>;
+  
+}
---
+++ src/layouts/LoginScreen.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation,useRootNavigationState } from "expo-router";
+import RegisterScreen from '../RegisterScreen';
+
+
+export default function LoginScreen(){
+    const navigation = useNavigation();
+    const [showRegister, setShowRegister] = useState(false)
+    useEffect(()=> {
+      navigation.setOptions({headerShown: false});
+    },[navigation]);
+
+    if(showRegister){
+      return <RegisterScreen onClose={()=> setShowRegister(false)}/>
+    }
+  
+    return (
+        <View style={styles.container}>
+          <Text style={styles.title}>Sign in </Text>
+          <TextInput 
+          style={styles.input}
+          placeholder="mmquintero@gmail.com"/>
+
+          <TextInput 
+          style={styles.input}
+          placeholder="********"
+          secureTextEntry/>
+
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttontext}> Login</Text>            
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}
+            onPress={()=>{
+              setShowRegister(true)
+            }}
+          >
+            <Text style={styles.buttontext}> reigster </Text>            
+          </TouchableOpacity>
+        </View>
+      );
+    }
+const styles = StyleSheet.create({
+      container: {
+        flex : 1,
+        justifyContent:"center",
+        alignItems: "center",
+        backgroundColor:"#95a5a6 "
+      },
+      title:{
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20
+
+      },
+      input:{
+        width: "80%",
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: "white",
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor:"#CCC"
+
+      },
+      button:{
+        marginTop: 20,
+        backgroundColor: "#007e80",
+        padding: 10,
+        borderRadius: 5,
+        width: "80%",
+        alignItems: "center"
+      },
+      buttontext:{
+        color: "white",
+        fontSize: 14,
+        fontWeight: "bold"
+      },
+    });
